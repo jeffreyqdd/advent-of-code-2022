44ba64b577276172afeec837d20a8546640ab500
Jeffrey Qian <jq54@cornell.edu>
2023-12-02 00:32:30 -0500

    updates to template main.py

--- template/main.py
+++ template/main.py
@@ -2,5 +2,9 @@
 from sys import stdin
 
 for line in stdin:
+    # strip newline and skip if line is empty (usually for newline char at EOF)
     line = line.strip()
+    if len(line) == 0:
+        continue
+
     print(line)
